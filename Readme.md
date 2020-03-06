1575459f7d0b9f9acf4c9ac8660ade9443006454
Rommel Santos <rommelrenne@gmail.com>
2020-03-05 22:48:56 -0300

    Added initial files of the project

---
+++ Readme.md
@@ -0,0 +1,10 @@
+#Pokemon Go Iv Calculator
+
+Just a simple IV Calculator for Pokemon Go
+
+Issues:
+
+[x] make tha base code of the calculator
+[ ] put an approximation closer to the correct value
+[ ] make an interface for the project
+[ ] make the pokemons
---
+++ iv_calculator.py
@@ -0,0 +1,24 @@
+##### IV CALCULATOR #####
+
+print('''
+
+	A Calculadora de IV a seguir mede o quanto seu Pokémon
+	é perfeito. Você consegue descobrir o grau de perfeição
+	indo no canto inferior direito da página do pokémon e 
+	clicando em "Avaliar"
+
+	Insira abaixo o nome do Pokémon e os resultados 
+	da avaliação:
+
+''')
+
+print(' ')
+
+pokemon = str(input('Pokémon: '))
+attack = int(input('Ataque (0 - 15): '))
+defense = int(input('Defesa (0 - 15): '))
+stamina = int(input('Stamina (0 - 15): '))
+
+iv_calc = ((attack + defense + stamina) * 100) / 45
+
+print(f'Seu {pokemon} é {iv_calc:.1f} % perfeito')
